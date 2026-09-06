9a7edf1e73a982cc940b4bea2ab215a1e6162a99
scillidan <scillidan@yandex.com>
2026-09-06 14:56:51 +0800

    .

--- win/startup-lite.cmd
+++ win/startup-lite.cmd
@@ -1,17 +1,9 @@
 start "" "%USERHOME%\Local\Data\StartMenu\Scoop Apps\Ditto.lnk"
 timeout /t 1 /nobreak >nul
-start "" "%USERHOME%\Local\Data\StartMenu\Scoop Apps\GoldenDict.lnk"
-timeout /t 1 /nobreak >nul
-start "" "%USERHOME%\Local\Data\StartMenu\Scoop Apps\GoldenDict Search In Group.lnk"
-timeout /t 1 /nobreak >nul
 start "" "%USERHOME%\Local\Data\StartMenu\Scoop Apps\Keyboard Auto Switch Back.lnk"
 timeout /t 1 /nobreak >nul
 start "" "%USERHOME%\Local\Data\StartMenu\Scoop Apps\Keyboard Switch.lnk"
 timeout /t 1 /nobreak >nul
 start "" "%USERHOME%\Local\Data\StartMenu\Scoop Apps\Keypirinha.lnk"
 timeout /t 1 /nobreak >nul
-start "" "%USERHOME%\Local\Data\StartMenu\Scoop Apps\RectangleWin.lnk"
-timeout /t 1 /nobreak >nul
 start "" "%USERHOME%\Local\Data\StartMenu\Scoop Apps\virgo.lnk"
-timeout /t 1 /nobreak >nul
-start "" "%USERHOME%\Local\Data\StartMenu\Scoop Apps\Window Switcher.lnk"
